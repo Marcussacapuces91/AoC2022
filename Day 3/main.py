8075d6540fe6f7fdb0e5e985928b1077e4d26eb7
Marc Sibert <marc@sibert.fr>
2022-12-03 13:30:45 +0100

    Update main.py

--- Day 3/main.py
+++ Day 3/main.py
@@ -8,22 +8,36 @@ def decoupe(_lines: list) -> Generator:
         yield line[:l2], line[-l2:]
 
 
+def valeur(char: str) -> int:
+    if char.islower():
+        return ord(char)-96
+    else:
+        return ord(char)-65+27
+    
+
 def response1(_lines: Generator) -> str:
     sum: int = 0
     for l, r in _lines:
         for c in l:
             if c in r:
-                if c.islower():
-                    sum += ord(c)-96
-                else:
-                    sum += ord(c)-65+27
+                sum += valeur(c)
                 break
             
     return f"Resultat 1 : {sum}"
 
 
-def response2(_lines: Generator) -> str:
-    return f"Resultat 2."
+def response2(_lines) -> str:
+    sum: int = 0
+    while len(_lines) > 0:
+        un = _lines.pop()
+        deux = _lines.pop()
+        trois = _lines.pop()
+        for c in un:
+            if c in deux and c in trois:
+                sum += valeur(c)
+                break
+        
+    return f"Resultat 2 : {sum}"
 
 
 if __name__ == '__main__':
@@ -35,7 +49,7 @@ if __name__ == '__main__':
         print(response1(decoupe(lines)))
         m = perf_counter_ns()
         print(f"Réponse en {(m - s) / 1000}ms.")
-        print(response2(decoupe(lines)))
+        print(response2(lines))
         e = perf_counter_ns()
         print(f"Réponse en {(e - m) / 1000}ms.")
         f.close()
